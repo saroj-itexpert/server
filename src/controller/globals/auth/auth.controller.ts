1702621d523be81081c7d31c97155de93d96c824
Saroj Maharjan <maharjan.saroj93@gmail.com>
2025-06-03 07:50:35 +0545

    encryption, decryption

---
+++ src/controller/globals/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+//individual feature haru yo folder ma rakhchhau
+exports.registerUser = ()=>{
+
+}
+
+class AuthController{
+    static async registerUser(){
+
+    }
+    static async loginUser(){
+
+    }
+}
+
+//class ko instantiate gareko object banaunu parcha
+// const AuthC = new AuthController()
+
+//static cha bhane sidhai exports gare pugcha
+module.exports = AuthController
